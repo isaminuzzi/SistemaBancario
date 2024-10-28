315405bafad909d0f31135a4da03f17f305525a4
Isadora Minuzzi Vieira <isadora.minuzzi@gmail.com>
2024-10-28 18:05:33 -0300

    ajuste de referencia

--- CartaoCredito/CreditCardApi/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ CartaoCredito/CreditCardApi/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91a1db07790392428c7eb554e86e829ee39db8b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f505d7fbfa8e43d9c2fce651f15d4adbba0eef7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- CartaoCredito/CreditCardApi/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ CartaoCredito/CreditCardApi/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91a1db07790392428c7eb554e86e829ee39db8b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f505d7fbfa8e43d9c2fce651f15d4adbba0eef7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
